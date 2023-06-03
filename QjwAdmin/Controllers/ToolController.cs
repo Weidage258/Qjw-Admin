4dfd9413440eb73b2af3cf703b5a68c110a78103
Wei <jiawei.qu@qct-china.com>
2023-06-03 17:27:09 +0800

    dbfisst 代码先行 通过反射获取项目下的类来创建表

--- QjwAdmin/Controllers/ToolController.cs
+++ QjwAdmin/Controllers/ToolController.cs
@@ -22,38 +22,40 @@ namespace QjwAdmin.Controllers
             //如果不存在则创建数据库
             _db.DbMaintenance.CreateDatabase();
             //创建表S
-            //string nspace = "Model.Entitys";
-            ////通过反射读取我们想要的类
-            //Type[] ass = Assembly.LoadFrom(AppContext.BaseDirectory + "Model.dll").GetTypes().Where(p => p.Namespace == nspace).ToArray();
-            //_db.CodeFirst.SetStringDefaultLength(200).InitTables(ass);
-            ////初始化炒鸡管理员和菜单
-            //Users user = new Users()
-            //{
-            //    Name = "admin",
-            //    NickName = "炒鸡管理员",
-            //    Password = "123456",
-            //    UserType = 0,
-            //    IsEnable = true,
-            //    Description = "数据库初始化时默认添加的炒鸡管理员",
-            //    CreateDate = DateTime.Now,
-            //    CreateUserId = 0,
-            //    IsDeleted = 0
-            //};
-            //long userId = _db.Insertable(user).ExecuteReturnBigIdentity();
-            //Menu menuRoot = new Menu()
-            //{
-            //    Name = "菜单管理",
-            //    Index = "menumanager",
-            //    FilePath = "../views/admin/menu/MenuManager",
-            //    ParentId = 0,
-            //    Order = 0,
-            //    IsEnable = true,
-            //    Description = "数据库初始化时默认添加的默认菜单",
-            //    CreateDate = DateTime.Now,
-            //    CreateUserId = userId,
-            //    IsDeleted = 0
-            //};
-            //_db.Insertable(menuRoot).ExecuteReturnBigIdentity();
+            string nspace = "Model.Entitys";
+            //通过反射读取我们想要的类
+            //获取当前路径的model.dll，获取想要的类
+            Type[] ass = Assembly.LoadFrom(AppContext.BaseDirectory + "Model.dll").GetTypes().Where(p => p.Namespace == nspace).ToArray();
+           //默认长度两百
+            _db.CodeFirst.SetStringDefaultLength(200).InitTables(ass);
+            //初始威大哥管理员和菜单
+            Users user = new Users()
+            {
+                Name = "admin",
+                NickName = "威大哥管理员",
+                Password = "123456",
+                UserType = 0,
+                IsEnable = true,
+                Description = "数据库初始化时默认添加的炒鸡管理员",
+                CreateDate = DateTime.Now,
+                CreateUserId = 0,
+                IsDeleted = 0
+            };
+            long userId = _db.Insertable(user).ExecuteReturnBigIdentity();
+            Menu menuRoot = new Menu()
+            {
+                Name = "菜单管理",
+                Index = "menumanager",
+                FilePath = "../views/admin/menu/MenuManager",
+                ParentId = 0,
+                Order = 0,
+                IsEnable = true,
+                Description = "数据库初始化时默认添加的默认菜单",
+                CreateDate = DateTime.Now,
+                CreateUserId = userId,
+                IsDeleted = 0
+            };
+            _db.Insertable(menuRoot).ExecuteReturnBigIdentity();
             return res;
           
         }
